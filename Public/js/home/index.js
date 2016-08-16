13915ca22643b3f1fb34e0db005e77fbb0cc37cb
heqiming <messikiller@aliyun.com>
2016-08-16 18:33:29 +0800

    add crousel

--- Public/js/home/index.js
+++ Public/js/home/index.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function() {
+    var interval = 2000;
+    var owl = $("#owl-demo");
+    owl.owlCarousel({
+      items : 10,
+    //   itemsDesktop : [1000,5],
+    //   itemsDesktopSmall : [900,3],
+    //   itemsTablet: [600,2],
+    //   itemsMobile : false
+    });
+    owl.trigger('owl.play', interval);
+});
